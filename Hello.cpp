af379c23578d1f5404b9eb93dcbf0a499f1a0955
Panithansayan <139094320+Panithansayan@users.noreply.github.com>
2023-07-10 10:39:19 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{	
+	cout<<"Studaent Code : 6606021620210"<<endl;
+	cout<<"Student Name  : Panithan Sayan"<<endl;
+	cout<<"     Division : Information Technology"<<endl;
+	cout<<"         Year : 1"<<endl;
+	cout<<"         Room : RA"<<endl;
+	cout<<"          Sex : Male"<<endl;
+	cout<<"          Age : 18"<<endl;
+	system("pause");
+    return (0);
+}     	
