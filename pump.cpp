1fcf7e312b8e26da617ab887ab7ece0b7262d71c
Panithansayan <139094320+Panithansayan@users.noreply.github.com>
2023-07-17 10:08:46 +0700

    Add files via upload

---
+++ pump.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string Name;
+	int brith ;
+	cout<<"Enter name :";
+	cin>>Name;
+	cout<<"Enter year of brith :";
+	cin>>brith;
+	cout<<"Your age =" <<2566-brith <<"year old \n";
+	system("pause");
+	return (0);
+}
